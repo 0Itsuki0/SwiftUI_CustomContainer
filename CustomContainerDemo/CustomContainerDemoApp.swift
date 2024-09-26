6ed52f76a0e8ea700ea5392516c2cb2746b4f0df
itsuki <itsuki.enjyoy@gmail.com>
2024-09-26 10:46:21 +0900

    Initial Commit

---
+++ CustomContainerDemo/CustomContainerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomContainerDemoApp.swift
+//  CustomContainerDemo
+//
+//  Created by Itsuki on 2024/09/26.
+//
+
+import SwiftUI
+
+@main
+struct CustomContainerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
